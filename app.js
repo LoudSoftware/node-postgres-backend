52354786ca083167aeba5f61f3e0cda0699bcf4e
LoudSoftware <nicoznam@gmail.com>
2018-04-03 14:13:50 -0400

    Removed useless files and added CORS security feature fix

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//Potentially fix cors issue
+var cors = require('cors');
+
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 
 var app = express();
@@ -23,8 +26,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Potential fix for cors errors
+app.use(cors());
+
+
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
